ed49783ff569c5be7e350148fa8b56bdd523c2ab
sravan <57430307+sravan1946@users.noreply.github.com>
2022-10-25 22:37:13 +0530

    enhancements

--- redditparser/redditparser.py
+++ redditparser/redditparser.py
@@ -1,9 +1,28 @@
+import discord
+import os
+import json
+import datetime
+import asyncio
+import requests #Playing both sides here hehe
+import aiohttp
+
+from bs4 import BeautifulSoup
+from pystreamable import StreamableApi
+
+import moviepy.editor as mpe
+from moviepy.video.io.ffmpeg_tools import *
+
+from redbot.core import commands
+from redbot.core.config import Config
+
+
 class Redditparser(commands.Cog):
     """The Reddit parser cog"""
 
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=718395193090375700, force_registration=True)
+        posthistory=[]
 
         default_guild = {
             "state": True,
@@ -19,6 +38,19 @@ class Redditparser(commands.Cog):
 
         self.config.register_guild(**default_guild)
         self.config.register_guild(**default_global)
+
+    async def upload_media(self, media, title):
+        
+        mail = await self.config.mail()
+        password = await self.config.password()
+
+        api = StreamableApi(mail, password)
+
+        vid = dict(api.upload_video(media, title))
+
+        await asyncio.sleep(20)
+        os.remove(media)     
+        return vid["shortcode"]
 
     def message_parser(self, message):
         redditlinks = []
@@ -59,19 +91,6 @@ class Redditparser(commands.Cog):
             content = f"<{image}>"
 
         await ctx.send(content=content, embed=embed)
-        
-        async def upload_media(self, media, title):
-        
-        mail = await self.config.mail()
-        password = await self.config.password()
-
-        api = StreamableApi(mail, password)
-
-        vid = dict(api.upload_video(media, title))
-
-        await asyncio.sleep(20)
-        os.remove(media)     
-        return vid["shortcode"]
 
     async def get_text(self, ctx, json_data, post_id):
         document = json_data['posts']['models'][post_id]['media']['richtextContent']['document'][0]
@@ -87,6 +106,116 @@ class Redditparser(commands.Cog):
         embed.add_field(name=f"Subreddit:", value=f"r/{subbreddit}", inline=True)
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
 
+        await ctx.send(embed=embed)
+
+    async def get_video(self, ctx, json_data, post_id):
+        path = f"C:\\Users\\{os.getlogin()}\\Downloads\\"
+        headers = {'User-Agent':'Mozilla/5.0'}
+
+        type = json_data['posts']['models'][post_id]['media']['type']
+
+        title = json_data['posts']['models'][post_id]['title']
+        author = json_data['posts']['models'][post_id]['author']
+        subbreddit = json_data['posts']['models'][post_id]['subreddit']['name']
+        spoiler = json_data['posts']['models'][post_id]['isSpoiler']
+        permalink = json_data['posts']['models'][post_id]['permalink']
+
+        embed=discord.Embed(description=f"**[{title}]({permalink})**",color=discord.Color.random())        
+        embed.add_field(name=f"Author:", value=f"u/{author}", inline=True)
+        embed.add_field(name=f"Subreddit:", value=f"r/{subbreddit}", inline=True)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        
+        if type == "video":
+            dash_url = json_data['posts']['models'][post_id]['media']['dashUrl']
+            height  = json_data['posts']['models'][post_id]['media']['height']
+            dash_url = dash_url[:int(dash_url.find('DASH')) + 4]
+            video_url = f'{dash_url}_{height}.mp4'
+            audio_url = f'{dash_url}_audio.mp4'
+            
+            with open(path + 'video.mp4','wb') as file:
+                response = requests.get(video_url,headers=headers)
+                if(response.status_code == 200):
+                    file.write(response.content)
+                    file.close()
+                else:
+                    file.close()
+
+            with open(path + 'audio.mp3','wb') as file:
+                response = requests.get(audio_url,headers=headers)
+                if(response.status_code == 200):
+                    file.write(response.content)
+                    file.close()
+                else:
+                    file.close()
+
+            def combine_audio(vidname, audname, outname):
+                ffmpeg_merge_video_audio(vidname, audname, outname, vcodec='copy', acodec='copy', ffmpeg_output=False, logger=None)
+                
+            try:
+                combine_audio(path + 'video.mp4', path + 'audio.mp3', path + 'final_video.mp4')
+                assert os.path.isfile(path + 'video.mp4')
+                assert os.path.isfile(path + 'audio.mp3')
+                os.remove(path + 'video.mp4')
+                os.remove(path + "audio.mp3")
+            except:
+                os.rename(path + "video.mp4", path+"final_video.mp4")
+                assert os.path.isfile(path + 'audio.mp3')
+                os.remove(path + "audio.mp3")
+
+        elif type == "gifvideo":
+            print("gif")
+            gif_url = json_data['posts']['models'][post_id]['media']['content']
+            with open(path + 'final_video.mp4','wb') as file:
+                response = requests.get(gif_url,headers=headers)
+                if(response.status_code == 200):
+                    file.write(response.content)
+                    file.close()
+                else:
+                    file.close()
+
+        if os.stat(path + 'final_video.mp4').st_size < 8000000:
+            file=discord.File(path + 'final_video.mp4', spoiler=spoiler)
+            await ctx.send(embed=embed,file=file)
+            os.remove(path + 'final_video.mp4')
+
+        else:
+            await asyncio.sleep(5) # To fix weird permission errors with windows
+            newname = f"streamable_{datetime.datetime.now().timestamp()}.mp4"
+            os.rename(path + 'final_video.mp4', path + newname)
+            code = await self.upload_media(path + newname, title)
+           
+            if code != None:
+                await ctx.send(content=f"https://streamable.com/{code}")
+                await ctx.send(embed=embed)
+            else:
+                code = "Streamable could not process this video"
+                await ctx.send(content=code,embed=embed)
+
+    async def get_url(self, ctx, json_data, post_id):
+        
+        source = json_data['posts']['models'][post_id]['source']['url']
+
+        title = json_data['posts']['models'][post_id]['title']
+        author = json_data['posts']['models'][post_id]['author']
+        subbreddit = json_data['posts']['models'][post_id]['subreddit']['name']
+        spoiler = json_data['posts']['models'][post_id]['isSpoiler']
+        permalink = json_data['posts']['models'][post_id]['permalink']
+
+        embed=discord.Embed(description=f"**[{title}]({permalink})**",color=discord.Color.random())        
+        embed.add_field(name=f"Author:", value=f"u/{author}", inline=True)
+        embed.add_field(name=f"Subreddit:", value=f"r/{subbreddit}", inline=True)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+
+        if source != None:
+            content=source
+
+        else:
+            print("Type not registered")
+
+        if spoiler == True:
+            context = f"|| {context} ||"
+
+        await ctx.send(content=content)
         await ctx.send(embed=embed)
 
     async def get_post(self, ctx, url):
@@ -113,12 +242,18 @@ class Redditparser(commands.Cog):
                     print(type)
                     if type == "image":
                         await self.get_image(ctx, json_data, post_id)
+                    if type in ["video","gifvideo"]:
+                        await self.get_video(ctx, json_data, post_id)
                     if type == "rtjson":
                         print("RTJSON")
                         await self.get_text(ctx, json_data, post_id)
+                    if type in ["url","embed"]:
+                        await self.get_url(ctx, json_data, post_id)
                         
                 else:    
                     await ctx.send("This post is NSFW, please use this command in a NSFW channel.")
+
+        await session.close()
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -209,3 +344,29 @@ class Redditparser(commands.Cog):
                 channels.remove(channelID)
                 await ctx.send(f"Reddit links will no longer be parsed in <#{channelID}>")
 
+    @commands.is_owner()
+    @rpsettings.command()
+    async def mail(self, ctx, *, ID:str):
+        """
+        This command is to add glyfcat id
+        """
+        await self.config.mail.set(ID)
+        await ctx.send("Streamable Mail set")
+
+    @commands.is_owner()
+    @rpsettings.command()
+    async def password(self, ctx, *, secret:str):
+        """
+        This command is to add glyfcat secret
+        """
+        await self.config.password.set(secret)
+        await ctx.send("Streamable Password set")
+
+    @commands.is_owner()
+    @rpsettings.command()
+    async def nukeconfig(self, ctx):
+        """
+        This command nukes the config
+        """
+        await self.config.clear_all()
+        await ctx.send("Config cleared")
